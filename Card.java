9c199ff9008a06890c7d2675f02d336c413ea71e
Marius <subrinmarius@gmail.com>
2025-11-29 12:18:16 +0100

    Class Player et Card
    En respectant le diagramme de classe j'ai fais les classes player et card, il manque la boucle de jeu, peut être une classe pour la game entière (avec les listes de joueurs et de cartes par exemple), les classes qui héritent de card et l'interface, youpi

---
+++ Card.java
@@ -0,0 +1,66 @@
+public abstract class Card {
+
+    // Enum pour représenter l'état de la carte
+    public enum State {
+        CACHEE,    // carte mise de côté au début
+        DANS_PIOCHE,
+        DANS_MAIN,
+        DEFAUSSEE
+    }
+
+    private static int compteurId = 0;
+
+    protected int idCard;
+    protected String nameCard;
+    protected State stateCard;
+
+
+    public Card(String nameCard) {
+        this.idCard = compteurId++;
+        this.nameCard = nameCard;
+        this.stateCard = State.DANS_PIOCHE; // par défaut : dans la pioche
+    }
+
+    // Getters
+    public int getIdCard() {
+        return this.idCard;
+    }
+
+    public String getNameCard() {
+        return this.nameCard;
+    }
+
+    public State getStateCard() {
+        return this.stateCard;
+    }
+
+    // Méthodes pour changer l'état
+    public void mettreDansPioche() {
+        this.stateCard = State.DANS_PIOCHE;
+    }
+
+    public void mettreDansMain() {
+        this.stateCard = State.DANS_MAIN;
+    }
+
+    public void defausser() {
+        this.stateCard = State.DEFAUSSEE;
+    }
+
+    public void cacher() {
+        this.stateCard = State.CACHEE;
+    }
+
+    @Override //Pour afficher la carte au joueur
+    public String toString() {
+        return "Card{id=" + idCard +
+               ", name='" + nameCard + '\'' +
+               ", state=" + stateCard +
+               '}';
+    }
+
+    // Méthode "virtuelle pure" (méthode abstraite)
+    // Elle devra être définie dans chaque sous-classe 
+    public abstract void appliquerEffet(); 
+    //Paramètres à changer en fonction des cartes
+}
---
+++ Player.java
@@ -0,0 +1,85 @@
+public class Player {
+
+    // Compteur global pour attribuer des IDs uniques
+    private static int compteurId = 0;
+
+    // Attributs de la classe
+    private int id;
+    private String nom;
+    private int nombre_faveur;
+    private boolean elimine;
+    private boolean protection;
+    private boolean espionne_jouee;
+
+    public Player(String nom) {
+        this.id = compteurId++;   // ID unique auto-incrémenté
+        this.nom = nom;
+        this.nombre_faveur = 0;
+        this.elimine = false;
+        this.protection = false;
+        this.espionne_jouee = false;
+    }
+
+    // Getters (accesseurs)
+    public int getId() {
+        return this.id;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public int getNombreFaveur() {
+        return this.nombre_faveur;
+    }
+
+    public boolean isElimine() {
+        return this.elimine;
+    }
+
+    public boolean hasProtection() {
+        return this.protection;
+    }
+
+    public boolean isEspionneJouee() {
+        return this.espionne_jouee;
+    }
+
+    // Setters
+    // Pas besoin de setNom car initialisé au départ et non changeable par la suite
+
+    public void elimination() {
+        this.elimine = true;
+    }
+
+    public void resurrection() {
+        this.elimine = false;
+    }
+
+    public void espionneJouee() {
+        this.espionne_jouee = true;
+    }
+
+    public void resetEspionneJouee() {
+        this.espionne_jouee = false;
+    }
+
+    public void ajouterFaveur(int n) {
+        this.nombre_faveur += n;
+    }
+
+    public void protectionOn() {
+        this.protection = true;
+    }
+
+    public void protectionOff() {
+        this.protection = false;
+    }
+
+    public void newRound() {
+        this.resurrection();
+        this.protectionOff();
+        this.resetEspionneJouee();
+    }
+    // A chaque début de manche il faudra utiliser newRound pour chaque joueur
+}
