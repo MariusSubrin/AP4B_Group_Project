5f160655d36ff7efc37e95151023e1a319d025e3
FafaGr <grief.fares@gmail.com>
2025-12-04 10:25:32 +0100

    Card.java
    - Ajout de l'attribut value pour les cartes afin de déterminer la valeur d'une carte.

--- Card.java
+++ Card.java
@@ -13,11 +13,13 @@ public abstract class Card {
     protected int idCard;
     protected String nameCard;
     protected State stateCard;
+    protected int valueCard;
 
 
-    public Card(String nameCard) {
+    public Card(String nameCard,int valueCard) {
         this.idCard = compteurId++;
         this.nameCard = nameCard;
+        this.valueCard = valueCard;
         this.stateCard = State.DANS_PIOCHE; // par défaut : dans la pioche
     }
 
@@ -32,6 +34,10 @@ public abstract class Card {
 
     public State getStateCard() {
         return this.stateCard;
+    }
+
+    public int getValueCard() {
+        return this.valueCard;
     }
 
     // Méthodes pour changer l'état
