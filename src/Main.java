e2076a030638b8275f207236828b81d6a331819f
FafaGr <grief.fares@gmail.com>
2026-01-09 19:23:08 +0100

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,15 @@ public class Main {
 }
 
 /*
-Soucis lorsque qu'on joue notre princesse ?
+Listes de tests à faire (non compléte) :
+- Utilisation de toutes les cartes en lançant le jeu
+
+Soucis lorsque qu'on joue notre dame
+
+Revoir les consignes pour savoir si tout est bien fait
+- Gestion des exceptions
+
+Diagramme de Gantt
  */
 
 /*
